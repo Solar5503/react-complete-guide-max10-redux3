265cc7dbc327709ad695c274a98120e39b11e2ec
Solar5503 <spidervvv@mail.ru>
2022-08-17 00:01:03 +0300

    handle cart with redux

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,11 @@
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { title, price, description, id } = props;
+  const dispatch = useDispatch();
 
   return (
     <li className={classes.item}>
@@ -13,7 +16,13 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button
+            onClick={() =>
+              dispatch(cartActions.addItemToCart({ id, title, price }))
+            }
+          >
+            Add to Cart
+          </button>
         </div>
       </Card>
     </li>
